ba6b1e3a89fc1a29b1c82becbfecb4db366ce54f
EsraaRamadan15 <esraa.ramadan2345@gmail.com>
2024-10-14 22:59:46 +0300

    Create product api without datbase handling

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
@@ -11,6 +9,19 @@ public class CreateProductEndPoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"products/{response.Id}", response);
+
+        })
+        .WithName("Create Product")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,16 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 //---- add services to container
 var app = builder.Build();
 
 //--- Configure HTTP request pipline
+
+app.MapCarter();
 app.Run();

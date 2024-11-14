2b28b56ab5061c44ecd29d7e11e05a7e5cc760d0
EsraaRamadan15 <esraa.ramadan2345@gmail.com>
2024-11-15 01:20:12 +0200

    Add Basket project with its features

---
+++ src/Services/Basket/Basket.API/Data/BasketRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Basket.API.Data
+{
+    public class BasketRepository(IDocumentSession session) : IBasketRepository
+    {
+
+        public async Task<bool> DeleteBasket(string username, CancellationToken cancellationToken)
+        {
+            session.Delete<ShoppingCart>(username);
+            await session.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<ShoppingCart> GetBasket(string username, CancellationToken cancellationToken)
+        {
+            var basket = await session.LoadAsync<ShoppingCart>(username, cancellationToken);
+
+            return basket is null ? throw new BasketNotFoundException(username) : basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart cart, CancellationToken cancellationToken)
+        {
+            session.Store(cart);
+            await session.SaveChangesAsync();
+            return cart;
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,36 @@
+
+
+using BuildingBlocks.Exceptions.Handler;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+//Cross-Cutting Services
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+// -- add services
+var app = builder.Build();
+
+
+//--- configure pipeline
+
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.Run();

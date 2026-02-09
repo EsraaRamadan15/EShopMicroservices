10595e9674680a766d5e17b2f5f32dec5b26fc6d
Esraa Ramadan <Esraa.Ramadan@obeikan.com.sa>
2026-02-09 20:33:35 +0200

    Add and prepare Shopping Web

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Web.Models.Catalog
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public record GetProductsByCateogryResponse(IEnumerable<Product> Products);
+
+
+    public record GetProductByIdResponse(Product Product);
+}
